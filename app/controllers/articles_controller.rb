ea4bc9ce8b2bbb84566d9a0d7be10591ecb5f909
wjhunter5315 <wjhunter5315@gmail.com>
2020-05-29 13:20:46 -0700

    added functional saving and displaying of new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,9 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to article_path(@article) #OR just redirect_to @article
     end
 
 end
