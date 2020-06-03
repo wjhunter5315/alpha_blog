42e5235f30ef0e81982c61a5c9980a6b7ca7929a
wjhunter5315 <wjhunter5315@gmail.com>
2020-06-03 14:09:31 -0700

    made an errors partial, flash alters with messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Disables the creation of a new <div> when an error occurs
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+end
