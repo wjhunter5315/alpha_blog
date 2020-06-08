420094777ea0df29957102dcf2cd80da6521e7ce
wjhunter5315 <wjhunter5315@gmail.com>
2020-06-08 16:26:35 -0700

    Add users create action, signup page, and jumbotron link

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+   def new
+      @user = User.new
+   end
+
+   def create
+      @user = User.new(user_params)
+      if @user.save
+         flash[:notice] = "Success! #{@user.username}, Welcome to Alpha Blog"
+         redirect_to articles_path
+      else
+         render 'new'
+      end
+   end
+
+   private
+   def user_params
+      params.require(:user).permit(:username, :email, :password)
+   end
+
+end
